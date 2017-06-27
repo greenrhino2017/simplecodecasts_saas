60885062ca8e9898ba1c641ee8e27fe286c7bbda
greenrhino2017 <greenrhino2017@outlook.com>
2017-06-27 03:33:50 +0000

    Fixed the users.js file

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,4 @@
-$(document).ready() {
+$(document).ready(function() {
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
   //Watch for a form submission
   $("#form-submit-btn").click(function(event) {
